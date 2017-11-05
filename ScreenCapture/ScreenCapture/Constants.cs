634a35742f398a1040b2b5c89f704ae52bfbc83a
Tyler Donaldson <tylerdonaldson92@gmail.com>
2017-11-05 10:22:53 -0500

    Made upload asynchronous

--- ScreenCapture/ScreenCapture/Constants.cs
+++ ScreenCapture/ScreenCapture/Constants.cs
@@ -2,6 +2,8 @@
 {
     public static class Constants
     {
+        public const string GyazoApiAccessToken = "784b0ae61c12329c359f43daf40d8a10d8520247731d6ec1cc415fcac116d61c";
+
         public static class SettingsKey
         {
             public const string SaveLocation = "SaveLocation";
